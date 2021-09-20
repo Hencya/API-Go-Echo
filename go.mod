56d9fd1e5b3a61f18a359b246fb7a8cc876d8e69
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2021-09-20 08:47:36 +0700

    data retrieve

--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module echo
 
 go 1.17
 
+require github.com/labstack/echo/v4 v4.5.0
+
 require (
-	github.com/labstack/echo/v4 v4.5.0 // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
--- main.go
+++ main.go
@@ -3,22 +3,71 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"strconv"
+	"time"
 )
+
+type article struct {
+	ID        int `json:"id"`
+	Name      string `json:"name"`
+	CreatedAt time.Time
+}
+
+var articles = []article{
+	{
+		ID:        1,
+		Name:      "Article_1",
+		CreatedAt: time.Now(),
+	},
+	{
+		ID:        2,
+		Name:      "Article_2",
+		CreatedAt: time.Now().AddDate(0, 0, -2),
+	},
+}
+
 
 func main() {
 	echoApp := echo.New()
 
-	echoApp.GET("/Hello",HelloworldController)
+	echoApp.GET("/articles",getArticleController)
+	echoApp.GET("/article",getArticleByQueryController)
+	echoApp.POST("/article",createArticleController)
+	echoApp.GET("/article/:id",getArticleByIdController)
 
 	echoApp.Start(":8080")
 }
 
-func HelloworldController(ctx echo.Context) error{
-	response := struct {
-		Data string `json:"data"`
-	}{
-		Data : "Hello World",
-	}
-	return ctx.JSON(http.StatusOK,response)
+func getArticleController(ctx echo.Context) error{
+	return ctx.JSON(http.StatusOK,articles)
 }
 
+func getArticleByIdController(ctx echo.Context) error{
+	//url params
+	id,_ := strconv.Atoi(ctx.Param("id"))
+	return ctx.JSON(http.StatusOK,articles[id-1])
+}
+
+func getArticleByQueryController(ctx echo.Context) error{
+	//query params
+	id,_ := strconv.Atoi(ctx.QueryParam("id"))
+	return ctx.JSON(http.StatusOK,articles[id-1])
+}
+
+func createArticleController(ctx echo.Context)error{
+	var newArticle article
+
+	formID,_ := strconv.Atoi(ctx.FormValue("id"))
+	formName := ctx.FormValue("name")
+
+	newArticle.ID = formID
+	newArticle.Name = formName
+	newArticle.CreatedAt = time.Now()
+
+	articles = append(articles,newArticle)
+
+	return ctx.JSON(http.StatusOK,map[string]interface{}{
+		"message": "succes create article",
+		"data": newArticle,
+	})
+}
